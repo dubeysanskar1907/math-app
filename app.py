d302bf050b871118661cb4293cbb90e7dedfa57c
dubeysanskar1907 <sanskardubey1907@gmail.com>
2026-07-02 16:11:25 +0530

    Add multiplication feature

--- app.py
+++ app.py
@@ -2,5 +2,6 @@ print("--- Math Application ---")
 num1 = float(input("Enter the first number: "))
 num2 = float(input("Enter the second number: "))
 
-print(f"\nResult:")
+print(f"\nResults:")
 print(f"Addition: {num1} + {num2} = {num1 + num2}")
+print(f"Multiplication: {num1} * {num2} = {num1 * num2}")
