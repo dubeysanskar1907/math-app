0ef4d9861d4faf27cc13a6e2ed479272631b93be
dev3 <dev3@gmail.com>
2026-07-02 15:52:01 +0530

    code added

---
+++ app.py
@@ -0,0 +1,6 @@
+print("--- Math Application ---")
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+print(f"\nResult:")
+print(f"Addition: {num1} + {num2} = {num1 + num2}")
